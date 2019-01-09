0c8f3edb20cdb7b09ab2ee68c8ef823757be0c4b
Alexandre Galvão Vida <alexandre.vida@valecard.com.br>
2019-01-09 18:06:28 -0200

    Atualizando Dev

--- src/teste.java
+++ src/teste.java
@@ -17,8 +17,11 @@ public class teste {
 
 	public static void main(String[] args) throws IOException, ParseException {
 		
-		//String nome = "/home/alexandre/Downloads/QPczEdsF.json";
-		String nome = "/home/alexandre/Downloads/sgo8GdIi.json";
+		//Casa
+		//String nome = "/home/alexandre/gitHub/jsontrello/Files/sgo8GdIi.json"
+		
+		//ValeCard
+		String nome = "/home/alexandre/gitHub/jsontrello/Files/sgo8GdIi.json";
 		
 		JSONParser parser = new JSONParser();
 		Reader reader = new FileReader(nome);
@@ -27,21 +30,77 @@ public class teste {
 
 		JSONObject jsonObject = (JSONObject) jsonObj;
 
-		String name = (String) jsonObject.get("id");
-		System.out.println("ID = " + name);
-
-//		String age = (String) jsonObject.get("name");
-//		System.out.println("Name = " + age);
-		
-//		String desc = (String) jsonObject.get("descData");
-//		System.out.println("desc = " + desc);
-
-		JSONArray cities = (JSONArray) jsonObject.get("actions");
+		JSONArray actions = (JSONArray) jsonObject.get("actions");
 		@SuppressWarnings("unchecked")
-		Iterator<String> it = cities.iterator();
-		while (it.hasNext()) {
-			System.out.println("actions = " + it.next());
-		}
+		Iterator<String> it = actions.iterator();
+		System.out.println("Actions " + actions.toString());
+		
+				
+		JSONParser parser2 = new JSONParser();
+		
+		String testeActions = actions.toString();
+		testeActions = testeActions.replace("[", "");
+		testeActions = testeActions.replace("]", "");
+		System.out.println("Actions " + testeActions);
+
+		Object jsonObj2 = parser2.parse(testeActions);				
+		
+		//JSONObject jsonObject3 = (JSONObject) jsonObj2;
+		JSONArray jsonObject3 = (JSONArray) jsonObj2;
+		@SuppressWarnings("unchecked")
+		Iterator<String> it2 = jsonObject3.iterator();
+		System.out.println("jsonObject3 " + jsonObject3.toString());
+
+		
+		
+				
+////		JSONParser parser3 = new JSONParser();
+//		Object jsonObj3 = (jsonObject3.get("data"));
+//		System.out.println("Data " + jsonObject3.toString());		
+//		
+//		JSONObject jsonObject4 = (JSONObject) jsonObj3;
+//		
+////		JSONParser parser4 = new JSONParser();
+//		Object jsonObj4 = (jsonObject4.get("list"));
+//		System.out.println("List " + jsonObject4.toString());
+//
+//		
+//		JSONObject jsonObject5 = (JSONObject) jsonObj4;
+//		
+//		String name = (String) jsonObject5.get("name");
+//		System.out.println("name = " + name);
+
+
+
+
+		
+		
+		
+		
+
+		
+		
+		
+		
+//		JSONArray cards = (JSONArray) jsonObject2.get("text");
+//		@SuppressWarnings("unchecked")
+//		Iterator<String> it2 = cards.iterator();
+//		System.out.println("Data " + cards.toString());
+		
+		
+//		JSONArray data = (JSONArray) jsonObject2.get("data");
+//		@SuppressWarnings("unchecked")
+//		Iterator<String> it2 = data.iterator();
+//		System.out.println("Date " + data.toString());
+		
+
+
+
+
+		
+		
+
+				
 		reader.close();
 	}
 	
